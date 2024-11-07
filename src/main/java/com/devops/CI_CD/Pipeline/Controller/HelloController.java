84ade4afbadc03b367e2750a4093171f9cac7e00
heshith <heshith619@gmail.com>
2024-11-07 07:47:55 +0530

    2nd test today

--- src/main/java/com/devops/CI_CD/Pipeline/Controller/HelloController.java
+++ src/main/java/com/devops/CI_CD/Pipeline/Controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello!! CI/CD ppe.....";
+        return "Helloooo!! CI/CD ppe.....";
     }
 }
